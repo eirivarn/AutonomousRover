461328ed972b137fbd9b358a513388814a654c5c
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-08 10:15:02 +0100

    lol

--- RoverVerstappen/Main.py
+++ RoverVerstappen/Main.py
@@ -1,9 +1,4 @@
-from time import sleep
-
-import cv2
-import Constants as Const
 from Rover import RoverHandler
-from Camera import PanTiltPiCamera
 
 if __name__ == "__main__":
     print("\n\nStarting!\n")
