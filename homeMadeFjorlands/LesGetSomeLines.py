e4c145474d8ca47b40b7ed358bde04ca87fb0463
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-10 14:16:08 +0100

    bugfix

--- homeMadeFjorlands/LesGetSomeLines.py
+++ homeMadeFjorlands/LesGetSomeLines.py
@@ -13,9 +13,8 @@ class LesGetSomeLines:
         self.rawCapture = PiRGBArray(self.camera, size=(640, 368))
         time.sleep(0.1)
         self.font = cv2.FONT_HERSHEY_SIMPLEX
-        self.Images=[]
         self.N_SLICES = 4
-        self.direction = 0
+        
 
 
     def startVideoCapture(self):
@@ -25,17 +24,19 @@ class LesGetSomeLines:
 
             image = frame.array
             removedBgImg = RemoveBackground(image, False)
+            images = []
+            direction = 0
             
             for _ in range(self.N_SLICES):
                 img = Image()
-                self.Images.append(img)
+                images.append(img)
             
             if removedBgImg is not None:
-                SlicePart(removedBgImg, self.Images, self.N_SLICES)
+                SlicePart(removedBgImg, images, self.N_SLICES)
                 for i in range(self.N_SLICES):
-                    self.direction += self.Images[i].getDir()
+                    direction += images[i].getDir()
                 
-                repackedImg = RepackImages(self.Images)
+                repackedImg = RepackImages(images)
             
             cv2.imshow('Image', repackedImg)
             self.rawCapture.truncate(0)
