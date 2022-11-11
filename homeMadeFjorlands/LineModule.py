6720b909fcc8f5463f54ad75d340ca252e16208e
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-11 15:57:23 +0100

    ..

--- homeMadeFjorlands/LineModule.py
+++ homeMadeFjorlands/LineModule.py
@@ -11,8 +11,9 @@ class LineModule:
         self.isHeadless = isHeadless
         self.camera = camera
         
-        self.rawCapture = PiRGBArray(self.camera, size=(640, 368))
-        time.sleep(0.1)
+        #self.rawCapture = PiRGBArray(self.camera, size=(640, 368))
+        #time.sleep(0.1)
+
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         self.N_SLICES = 4
         self.images = []
@@ -22,27 +23,27 @@ class LineModule:
         
 
 
-    def startVideoCapture(self):
-        time.sleep(0.0001)
-        for frame in self.camera.capture_continuous(self.rawCapture, format=("bgr"), use_video_port=True):
-            time.sleep(0.0001)
+    def analyzeImage(self, image):
+        #time.sleep(0.0001)
+        #for frame in self.camera.capture_continuous(self.rawCapture, format=("bgr"), use_video_port=True):
+            #time.sleep(0.0001)
 
-            image = frame.array
-            removedBgImg = RemoveBackground(image, True)
-            direction = 0
+            #image = frame.array
+        removedBgImg = RemoveBackground(image, True)
+        direction = 0
+        
+        if removedBgImg is not None:
+            SlicePart(removedBgImg, self.images, self.N_SLICES)
+            for i in range(self.N_SLICES):
+                direction += self.images[i].getDir()
             
-            if removedBgImg is not None:
-                SlicePart(removedBgImg, self.images, self.N_SLICES)
-                for i in range(self.N_SLICES):
-                    direction += self.images[i].getDir()
-                
-                repackedImg = RepackImages(self.images)
-            
-            printInfo(self.images)
+            repackedImg = RepackImages(self.images)
+        
+        printInfo(self.images)
 
-            if not self.isHeadless:
-                cv2.imshow('Image', repackedImg)
-            self.rawCapture.truncate(0)
+        if not self.isHeadless:
+            cv2.imshow('Image', repackedImg)
+        self.rawCapture.truncate(0)
            
             if cv2.waitKey(1) & 0xff == ord('q'):
                 break
