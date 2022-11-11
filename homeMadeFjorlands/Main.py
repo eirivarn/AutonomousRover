889700fa0f30c5067c4f334f2598b65247ba4dbd
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-11 10:16:37 +0100

    ..

--- homeMadeFjorlands/Main.py
+++ homeMadeFjorlands/Main.py
@@ -14,25 +14,29 @@ if __name__ == "__main__":
 
     motor = MotorControl()
     lineModule = LineModule(False)
-    cupModule = CupModule(False)
+    #cupModule = CupModule(False)
 
-    victory = False
+    def doCourse():
+        victory = False
 
-    task1 = Task1(motor, lineModule, cupModule)
-    task1.execute()
-    if task1.completed():
-        task2 = Task2(motor, lineModule)
-        task2.execute()
-        if task2.completed():
-            task3 = Task3(motor, lineModule)
-            task3.execute()
-            if task3.completed():
-                task4 = Task4(motor, lineModule)
-                task4.execute()
-                if task4.completed():
-                    Victory(motor, lineModule)
-                    vitory = True
-    if not vitory:
-        Fail(motor, lineModule)
+        task1 = Task1(motor, lineModule, cupModule)
+        task1.execute()
+        if task1.completed():
+            task2 = Task2(motor, lineModule)
+            task2.execute()
+            if task2.completed():
+                task3 = Task3(motor, lineModule)
+                task3.execute()
+                if task3.completed():
+                    task4 = Task4(motor, lineModule)
+                    task4.execute()
+                    if task4.completed():
+                        Victory(motor, lineModule)
+                        vitory = True
+        if not vitory:
+            Fail(motor, lineModule)
+    
 
     lineModule.startVideoCapture()
+
+    
