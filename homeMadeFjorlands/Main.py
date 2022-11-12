0f6c1c6f120d8e002b9ada7516e037a7d6fbd8c2
eirivarn <eirik.varnes@gmail.com>
2022-11-12 13:03:35 +0100

    ..

--- homeMadeFjorlands/Main.py
+++ homeMadeFjorlands/Main.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
 
     print("\n\nStarting!\n")
     robot = Rover()
-    image = robot.camera.startVideoCapture
+    image = robot.camera.startVideoCapture()
     robot.update(image)
