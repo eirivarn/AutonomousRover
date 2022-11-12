cbbdeb31098ac7a1f9f9e7e8f1af48f6678a04a7
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-12 22:56:13 +0100

    feil i main og startet på task 4

--- homeMadeFjorlands/Main.py
+++ homeMadeFjorlands/Main.py
@@ -3,6 +3,5 @@ from RobotController import Robot
 if __name__ == "__main__":
 
     print("\n\nStarting!\n")
-    robot = Rover()
-    image = robot.camera.startVideoCapture(robot)
-    robot.update(image)
+    robot = Robot()
+    robot.camera.startVideoCapture(robot)
--- homeMadeFjorlands/Task4.py
+++ homeMadeFjorlands/Task4.py
@@ -3,5 +3,61 @@ from Task import Task
 
 class Task4(Task):
 
+    def __init__(self, motorContorl, lineModule, cupModule):
+        super().__init__(motorContorl, lineModule, cupModule)
+        self.subTask = 1
+
     def update(self, image):
+        if self.subTask == 1:       #tilt camera up
+            self.subTask1(image)
+        
+        elif self.subTask == 2:     #go to cup
+            self.subTask2(image)
+            
+        elif self.subTask == 3:     #pick up cup
+            self.subTask3(image)
+            
+        elif self.subTask == 4:     #return to line
+            self.subTask4()
+                       
+        elif self.subTask == 5:     # turn 90 deg to main line
+            self.subTask5(image)
+
+
+    def subTask1(self, image):
         pass
+    def subTask2(self, image):
+        pass
+    def subTask3(self, image):
+        pass
+    def subTask4(self, image):
+        pass
+    def subTask5(self, image):
+        pass
+    def subTask6(self, image):
+        pass
+    def subTask7(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    def subTask(self, image):
+        pass
+    
