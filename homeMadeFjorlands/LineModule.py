6dd5dd25ded6094f1ff13956781b5cfdac1b58d4
eirivarn <eirik.varnes@gmail.com>
2022-11-12 14:15:19 +0100

    ..

--- homeMadeFjorlands/LineModule.py
+++ homeMadeFjorlands/LineModule.py
@@ -34,8 +34,8 @@ class LineModule:
 
         if not self.isHeadless:
             cv2.imshow('Image', repackedImg)
-        #self.rawCapture.truncate(0)  TODO finn ut om denne skal stå her
-        
+            self.rawCapture.truncate(0)
+
         crossFound = Image.crossFound(self.images)
 
         return line, crossFound #TODO    skal returne line - liste av avstand fra linje til senter av bildet
