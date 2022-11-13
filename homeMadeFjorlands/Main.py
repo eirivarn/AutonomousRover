b952ea8430550826d420bf0c038d2c1f48c20114
eirivarn <eirik.varnes@gmail.com>
2022-11-13 14:55:52 +0100

    funka ikje :/

--- homeMadeFjorlands/Main.py
+++ homeMadeFjorlands/Main.py
@@ -1,15 +1,9 @@
 from RobotController import Robot
-import keyboard
-from getkey import getkey, keys
 
 if __name__ == "__main__":
-    while True:
-        key_pressed = getkey(blocking=False)
-        if key_pressed == "q":
-            exit()
-        print("\n\nStarting!\n")
-        robot = Robot()
-        image = robot.camera.startVideoCapture(robot)
-        robot.update(image)
+    print("\n\nStarting!\n")
+    robot = Robot()
+    image = robot.camera.startVideoCapture(robot)
+    robot.update(image)
 
 
