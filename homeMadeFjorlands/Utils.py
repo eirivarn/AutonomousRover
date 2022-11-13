7437d8a9745f591de19946b55858c10584346d89
eirivarn <eirik.varnes@gmail.com>
2022-11-13 16:19:03 +0100

    YOLO2

--- homeMadeFjorlands/Utils.py
+++ homeMadeFjorlands/Utils.py
@@ -46,7 +46,44 @@ def RemoveBackground(image, b):
         return image
     else:
         return image
-    #////////////////COLOR SELECTION/////////////
+
+def improveLine(self,pic):
+        kernel = np.ones((3,3),np.uint8)
+        pic = cv2.erode(pic, kernel, iterations=4)
+        pic = cv2.dilate(pic, kernel, iterations=4)
+        return pic
+    
+def preProcessAngle(self,ang, w, h):
+    if ang > 45 and w>h:
+        ang = -(90-ang)
+    elif ang <= 45 and h<w:
+        ang = -(90-ang)
+    return ang
+
+def ekstraBox(image):
+    #differentiate black black areas
+        blackAreas = cv2.inRange(image, (0,0,0), (50,50,50))
+        blackAreas = self.improveLine(blackAreas)
+        blackContours, hierarchy = cv2.findContours(blackAreas.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(blackContours) > 0:
+            c = max(blackContours, key=cv2.contourArea) #biggest black contour area
+            xb, yb, wb, hb = cv2.boundingRect(c) #bounding box rectangle
+            contourbox = cv2.minAreaRect(c) #contour tape rectangle
+            (xc, yc), (wc,hc), angle = contourbox
+            #determine angle and lateral offset
+            angle = self.preProcessAngle(angle,wc,hc)
+            lateralOffset = int(320-xb-wb/2)
+            
+            #Write boxes and lines to image
+            cv2.rectangle(image,(xb,yb),(xb+wb,yb+hb),(0,255,0),2) #Bounding box
+            cv2.line(image, (int(xb+wb/2), yb), (int(xb+wb/2), yb+hb),(0,255,0), 2) #bounding box centerline
+            cv2.line(image,(int(xb+wb/2), int(yb+hb/2)), (320, int(yb+hb/2)),(0,0,255), 1)# distance line
+            cv2.line(image, (320, 10), (320, 350),(0,0,255), 1)#center line
+            cv2.drawContours(image, [np.int0(cv2.boxPoints(contourbox))],0,(255,0,0), 2)#draw contourBox
+            cv2.putText(image, f"{str(int(angle))}deg", (360,300),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)#text>
+            cv2.putText(image, str(lateralOffset)+"dist", (360,330),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)#te>
+            cv2.drawContours(image, blackContours,-1,(0,0,255),1)
+    
 
 
 #/////////////////prettyPrint///////////////
