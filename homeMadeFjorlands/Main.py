3ee065fb27b6dc66e3faa64c79173ea5c9bd4ccc
eirivarn <eirik.varnes@gmail.com>
2022-11-13 15:00:18 +0100

    forsøk 2

--- homeMadeFjorlands/Main.py
+++ homeMadeFjorlands/Main.py
@@ -1,9 +1,24 @@
 from RobotController import Robot
+from getkey import getkey
+from threading import Event, Thread
 
 if __name__ == "__main__":
-    print("\n\nStarting!\n")
-    robot = Robot()
-    image = robot.camera.startVideoCapture(robot)
-    robot.update(image)
+    SHUTDOWN = Event()
+
+    def shutdown_on_key():
+            while True:
+                key_pressed = getkey(blocking=True)
+                if key_pressed == 'q':
+                    SHUTDOWN.set()
+                    return
+
+    t = Thread(target=shutdown_on_key)
+    t.start()
+
+    while not SHUTDOWN.is_set():
+        print("\n\nStarting!\n")
+        robot = Robot()
+        image = robot.camera.startVideoCapture(robot)
+        robot.update(image)
 
 
