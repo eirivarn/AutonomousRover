142779c7867cb9a2e76f2bd7539c9b1be44e0e81
eirivarn <eirik.varnes@gmail.com>
2022-11-14 23:58:26 +0100

    gg

--- homeMadeFjorlands/LinReg.py
+++ homeMadeFjorlands/LinReg.py
@@ -1,75 +1,92 @@
-import pandas as pd
-import numpy as np
+# Linear regression from scratch
+def add(x, y):
+    if len(x) != len(y):
+        print("Dimention mismatch")
+        exit()
+    else:
+        z = [x[i] + y[i] for i in range(len(x))]
+    return z
 
-matrix = np.array(A.values,'float')
-matrix[0:5,:]    #first 5 rows of data
-data = np.array([[194.33333333,   7.526     ],
-       [188.33333333,   7.509     ],
-       [124.        ,   7.501     ],
-       [123.        ,   7.498     ],
-       [164.33333333,   7.413     ]])
 
-X = matrix[:,0]
-y = matrix[:,1]
+def sub(x, y):
+    if len(x) != len(y):
+        print("Dimention mismatch")
+        exit()
+    else:
+        z = [x[i] - y[i] for i in range(len(x))]
+    return z
 
-X = X/(np.max(X)) 
 
-import matplotlib.pyplot as plt
-plt.plot(X,y,'bo')
-plt.ylabel('Happiness Score')
-plt.xlabel('Alcohol consumption')
-plt.legend(['Happiness Score'])
-plt.title('Alcohol_Vs_Happiness')
-plt.grid()
-plt.show()
+def mul(x, y):
+    z = [x[i] * y[i] for i in range(len(x))]
+    return z
 
-def computecost(x,y,theta):
-    
-    a = 1/(2*m)
-    b = np.sum(((x@theta)-y)**2)
-    j = (a)*(b)
-    return j
 
-print(computecost(x,y,theta))
+def div(x, y):
+    if len(x) != len(y):
+        print("Dimention mismatch")
+        exit()
+    else:
+        z = [x[i] / y[i] for i in range(len(x))]
+    return z
 
-#initialising parameter
-m = np.size(y)
-X = X.reshape([122,1])
-x = np.hstack([np.ones_like(X),X])
-theta = np.zeros([2,1])
-print(theta,'\n',m)
 
-def gradient(x,y,theta):
-    
-    alpha = 0.00001
-    iteration = 2000
-#gradient descend algorithm
-    J_history = np.zeros([iteration, 1]);
-    for iter in range(0,2000):
-        
-        error = (x @ theta) -y
-        temp0 = theta[0] - ((alpha/m) * np.sum(error*x[:,0]))
-        temp1 = theta[1] - ((alpha/m) * np.sum(error*x[:,1]))
-        theta = np.array([temp0,temp1]).reshape(2,1)
-        J_history[iter] = (1 / (2*m) ) * (np.sum(((x @ theta)-y)**2))   #compute J value for each iteration 
-    return theta, J_history
+def pypow(x, y):
+    z = [x[i] ** y for i in range(len(x))]
+    return z
 
-theta , J = gradient(x,y,theta)
-print(theta)
 
-theta , J = gradient(x,y,theta)
-print(J)
+def ones1d(n):
+    z = []
+    for i in range(n):
+        z.append(1)
+    return z
 
-#plot linear fit for our theta
-plt.plot(X,y,'bo')
-plt.plot(X,x@theta,'-')
-plt.axis([0,1,3,7])
-plt.ylabel('Happiness Score')
-plt.xlabel('Alcohol consumption')
-plt.legend(['HAPPY','LinearFit'])
-plt.title('Alcohol_Vs_Happiness')
-plt.grid()
-plt.show()
 
-predict1 = [1,(164/np.max(matrix[:,0]))] @ theta #normalising the input value, 1 is for intercept term so not need to normalise
-print(predict1)
+def expand(val, n):
+    z = []
+    for i in range(n):
+        z.append(val)
+    return z
+
+
+def random1d(strt, end, n):
+    z = []
+    import random
+    for i in range(n):
+        val = random.randint(strt, end)
+        z.append(val)
+    return z
+
+
+def linear_regressor(x, y, lr, niter):  # x, y both are row vector
+    N = len(x)
+    # W = random1d(1, 20, N)
+    import random
+    W = expand(random.randint(1, 20), N)
+    b = []
+    for i in range(N):
+        b.append(0)
+
+    for i in range(niter):
+        ypred = add(mul(W, x), b)
+        L = mul(div(ones1d(N), expand(N, N)), expand(sum(pypow(sub(y, ypred), 2)), N))
+        # print(L)
+        dL_dW = mul(div(expand(-2, N), expand(N, N)), expand(sum(mul(sub(y, ypred), x)), N))
+        dL_db = mul(div(expand(-2, N), expand(N, N)), expand(sum(sub(y, ypred)), N))
+        # update weight
+        W = sub(W, mul(expand(lr, N), dL_dW))
+        # print(W)
+        b = sub(b, mul(expand(lr, N), dL_db))
+        print("MSE Loss is:" + str(L[0]))
+        print("Iteration:" + str(i))
+    # store result
+    global weight, bias
+    weight = W[0]
+    bias = b[0]
+    return ypred
+
+
+def linear_pred(x):  # prediction function
+    y = add(mul(expand(weight, len(x)), x), expand(bias, len(x)))
+    return y
