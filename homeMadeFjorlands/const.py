3039da369dbc48dc74b94e427872206203074eb6
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-14 13:43:10 +0100

    constanter

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -6,6 +6,7 @@ class Const:
         self.cupPosBuffer = 10
         self.lineDistBuffer = 2
         self.posDistBuffer = 2
+        self.need4speed = 70  #tør me å vinna??
 
         self.lSpeedPin = 7
         self.lDirPin = 11
@@ -14,3 +15,12 @@ class Const:
 
         self.kp = 0.1
         self.ap = 0.5
+
+        self.n_slices = 4
+        self.threshGrey = 65
+        self.crossWidth = 300
+        self.cupConfidence = 0.55
+        self.cupObjects = ['cup','bowl']
+        self.cupIsClose = 100
+
+        self.i_line = 2
