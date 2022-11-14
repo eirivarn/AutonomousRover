2a571e2e6aef50251c6a24f1ee10c0f9a2f34c94
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-14 16:10:48 +0100

    turnSpeed

--- homeMadeFjorlands/motorTest.py
+++ homeMadeFjorlands/motorTest.py
@@ -37,7 +37,7 @@ def test1(image):
     print("Executing subtask 2")
     cupPos, cupInImage, cupIsClose = cupModule.analyzeImage(image)
     if not cupInImage:
-        motorControl.turnLeft(80)
+        motorControl.turnLeft(50)
     else:
         motorControl.turnToPos(cupPos)
     if cupPos in range(-cupDistBuffer, cupDistBuffer):
