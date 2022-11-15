f1f60f925a5cfcdda6e58aca0b11166b70145a91
eirivarn <eirik.varnes@gmail.com>
2022-11-15 01:45:56 +0100

    ..

--- homeMadeFjorlands/LineModule.py
+++ homeMadeFjorlands/LineModule.py
@@ -43,7 +43,8 @@ class LineModule:
 
         m = (len(x) * np.sum(x*y) - np.sum(x) * np.sum(y)) / (len(x)*np.sum(x*x) - np.sum(x) ** 2)
         b = (np.sum(y) - m *np.sum(x)) / len(x)
-
+        print(m)
+        print(b)
         offset = (m*self.const.resolution[1]/2 - b) - self.const.resolution[0]/2
         print(offset)
         angle = np.arctan((m*10 - 2*b)/10)
