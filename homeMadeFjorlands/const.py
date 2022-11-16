80199c44a01974ace5fcea3bf0db86f4c64a37c9
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-16 18:20:23 +0100

    speed

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -2,8 +2,8 @@ import numpy as np
 
 class Const:
     def __init__(self):
-        self.speed = 20
-        self.turnSpeed = 20
+        self.speed = 30
+        self.turnSpeed = 30
         self.cupDistBuffer = 2
         self.cupPosBuffer = 70
         self.lineDistBuffer = 2
