598fe8eae8f27fc428743bf550ab8a338e73ed63
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-18 17:02:28 +0100

    ..

--- homeMadeFjorlands/LineModule.py
+++ homeMadeFjorlands/LineModule.py
@@ -72,7 +72,7 @@ class LineModule:
         if inversedAngle < 0:
             angle = -90 - inversedAngle
 
-        angle = -1
+        angle = -angle
         offset = list[-1]
         #offset = self.predict(self.const.resolution[1]/2)
 
