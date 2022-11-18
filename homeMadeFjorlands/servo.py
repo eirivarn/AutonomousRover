7b8fd989e5602dd981808c74f6a163edd47d800b
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-18 21:21:33 +0100

    serco class cleanup

--- homeMadeFjorlands/servo.py
+++ homeMadeFjorlands/servo.py
@@ -4,9 +4,9 @@ class servo:
        
     def __init__(self, port, angOrCont):
         self.port = port
-        self.open = 70
-        self.close = 120
-        self.up = 45
+        self.open_val = 0.6
+        self.close_val = 0.15
+        self.up = 0.2
         self.flat = 0
         self.cameraFront = 0
         self.cameraLeft = 1
@@ -19,27 +19,18 @@ class servo:
 
  
    
-    def open(self):
-        self.servo.value = self.open
+    def openGripper(self):
+        self.servo.value = self.open_val
  
-    def close(self):
-        self.servo.value = self.close
- 
+    def closeGripper(self):
+        self.servo.value = self.close_val
+
     def lift(self):
-        self.servo.angle = self.up
+        self.servo.value = self.up
  
     def lower(self):
-        self.servo.angle = self.flat
+        self.servo.value = self.flat
  
-    def panRight(self):
-        self.servo.value = self.cameraRight
-   
-    def panLeft(self):
-        self.servo.value = self.cameraRight
-   
-    def panFront(self):
-        self.servo.value = self.cameraRight
-        
         
         
 
