64b7e22fcc872913cc10c0f5026c70a0e6ae2e0d
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-18 11:36:06 +0100

    ..

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -31,7 +31,7 @@ class Const:
         self.i_line = 2
 
         self.resolution = (640, 380)
-        self.offsetPosition = 1.25 #regner offset fra litt utenfor skjermen, mot roboten
+        self.offsetPosition = 1.27 #regner offset fra litt utenfor skjermen, mot roboten
         self.linRegPlotY1 = 1
         #self.linRegPlotY2 = int(self.resolution[1] * self.offsetPosition)
         self.linRegPlotY2 = 379
