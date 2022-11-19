742df9e9391c90beeb1ea9a18d80748c9a3bd004
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-19 22:38:49 +0100

    ..

--- homeMadeFjorlands/servotester.py
+++ homeMadeFjorlands/servotester.py
@@ -12,13 +12,7 @@ for i in range(5):
     sleep(1)
     cameraServo.down()
     gripperServo.close()
-
     sleep(1)
-    cameraServo.up()
-    gripperServo.open()
-    sleep(1)
-    cameraServo.down()
-    gripperServo.close()
 
 cameraServo.detatch()
 gripperServo.detatch()
