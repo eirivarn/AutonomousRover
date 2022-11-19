d50a2fe88d0d7c6f088a10e351c67b315ec37c01
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-19 20:23:39 +0100

    ..

--- homeMadeFjorlands/servo.py
+++ homeMadeFjorlands/servo.py
@@ -1,9 +1,11 @@
 from gpiozero import AngularServo
+from gpiozero.pins.pigpio import PiGPIOFactory
 from const import Const
  
 class servo:
     def __init__(self, port):
-        self.servo = AngularServo(port)
+        factory = PiGPIOFactory()
+        self.servo = AngularServo(port, pin_factory = factory)
         self.const = Const()
 
     def openGripper(self):
