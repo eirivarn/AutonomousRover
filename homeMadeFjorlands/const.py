178e98ed5023a9566c9b60151105ac96812a8016
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-20 21:30:31 +0100

    ..

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -5,7 +5,7 @@ class Const:
         self.isHeadless = False
 
         self.speed = 16
-        self.turnSpeed = 48
+        self.turnSpeed = 45
         self.cupDistBuffer = 1
         self.cupPosBuffer = 70
         self.lineDistBuffer = 2
