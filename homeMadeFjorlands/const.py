a492d6b72f7cb2c9626b4fbf08be400fed7b3ed1
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-20 18:57:22 +0100

    turnspeed 35

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -5,7 +5,7 @@ class Const:
         self.isHeadless = False
 
         self.speed = 13
-        self.turnSpeed = 30
+        self.turnSpeed = 35
         self.cupDistBuffer = 2
         self.cupPosBuffer = 70
         self.lineDistBuffer = 2
