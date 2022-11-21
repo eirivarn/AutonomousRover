c7f17c8a9fb70680bea16f4222db117f6c55429a
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-21 18:08:05 +0100

    color black

--- homeMadeFjorlands/Utils.py
+++ homeMadeFjorlands/Utils.py
@@ -33,7 +33,7 @@ def Center(moments):
     return x, y
     
 def RemoveBackground(image, b):
-    up = 50
+    up = 30
     # create NumPy arrays from the boundaries
     lower = np.array([0, 0, 0], dtype = "uint8")
     upper = np.array([up, up, up], dtype = "uint8")
