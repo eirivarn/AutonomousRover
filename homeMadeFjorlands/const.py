316d43563e757f7412249d6e25a46e3aa34043d2
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-24 18:27:15 +0100

    ..

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -26,7 +26,7 @@ class Const:
 
         self.n_slices = 5
         self.threshGrey = 65
-        self.crossWidth = 300
+        self.crossWidth = 200
         self.cupConfidence = 0.55
         self.cupObjects = ['cup','bowl']
         self.cupIsClose = 55
