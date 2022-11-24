f83093dcbdb041148c5b71312f4d59de17036311
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-24 12:24:13 +0100

    ..

--- homeMadeFjorlands/DCmotor.py
+++ homeMadeFjorlands/DCmotor.py
@@ -8,7 +8,7 @@ class DCmotor:
         self.speedPin = speedPin
         self.directionPin = directionPin
         self.const = const
-        self.sleeping = False
+        #self.sleeping = False
  
         self.speed = 0
        
@@ -23,17 +23,17 @@ class DCmotor:
         if speed > 100:
             speed = 100
 
-        if not self.sleeping:
-            if speed >= 0:
-                if abs(speed-self.speed) < self.const.minSpeed4init and abs(speed)>abs(self.speed):
-                    self.initMotor(speed)
-                else:
-                    GPIO.output(self.directionPin, 1)
-                    self.speed_pwm.ChangeDutyCycle(speed)
-            else:
-                self.backward(-speed)
+        #if not self.sleeping:
+        if speed >= 0:
+            '''if abs(speed-self.speed) < self.const.minSpeed4init and abs(speed)>abs(self.speed):
+                self.initMotor(speed)
+            else:'''
+            GPIO.output(self.directionPin, 1)
+            self.speed_pwm.ChangeDutyCycle(speed)
+        else:
+            self.backward(-speed)
 
-            self._setSpeed(speed)
+        self._setSpeed(speed)
 
         
    
@@ -41,17 +41,17 @@ class DCmotor:
         if speed > 100:
             speed = 100
 
-        if not self.sleeping:
-            if speed >= 0:
-                if abs(speed-self.speed) < self.const.minSpeed4init and abs(speed)>abs(self.speed):
-                    self.initMotor(speed)
-                else:
-                    GPIO.output(self.directionPin, 0)
-                    self.speed_pwm.ChangeDutyCycle(speed)
-            else:
-                self.forward(-speed)
-            self._setSpeed(-speed)
-   
+        #if not self.sleeping:
+        if speed >= 0:
+            '''if abs(speed-self.speed) < self.const.minSpeed4init and abs(speed)>abs(self.speed):
+                self.initMotor(speed)
+            else:'''
+            GPIO.output(self.directionPin, 0)
+            self.speed_pwm.ChangeDutyCycle(speed)
+        else:
+            self.forward(-speed)
+        self._setSpeed(-speed)
+
  
     def quit(self):
         #print("quit")
@@ -69,17 +69,18 @@ class DCmotor:
     def getSpeed(self):
         return self.speed
 
-    def initMotor(self, speed):
+
+    '''def initMotor(self, speed):
         dir = 1 if speed >=0 else 0
 
         thread = threading.Thread(target=self.boozt, args=[dir])
-        thread.start()
+        thread.start()'''
     
-
+    '''
     def boozt(self, dir):
         GPIO.output(self.directionPin, dir)
         self.speed_pwm.ChangeDutyCycle(self.const.speedBoozt)
         self.sleeping = True
         sleep(self.const.initSleep)
-        self.sleeping = False
+        self.sleeping = False'''
 
