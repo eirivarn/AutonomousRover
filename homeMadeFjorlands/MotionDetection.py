adc1b982e1877b4b885dc3f1cf7aa45c1e5afe28
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-24 12:02:02 +0100

    ..

--- homeMadeFjorlands/MotionDetection.py
+++ homeMadeFjorlands/MotionDetection.py
@@ -14,8 +14,8 @@ class MotionDetection:
     def detectMotion(self, image):
     
         greyscale_frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
-        gaussian_frame = cv2.GaussianBlur(greyscale_frame, (11,11),0)
-        blur_frame = cv2.blur(gaussian_frame, (3,3)) 
+        gaussian_frame = cv2.GaussianBlur(greyscale_frame, (17,17),0)
+        blur_frame = cv2.blur(gaussian_frame, (4,4)) 
         greyscale_image = blur_frame 
 
         if self.lastFreame is None:
