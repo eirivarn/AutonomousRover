5ea48126f67569dbc539268749394c7118120eda
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-25 08:08:43 +0100

    8 slic

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -26,7 +26,7 @@ class Const:
         self.kd = 0.15
         self.ki = 0.0001
 
-        self.n_slices = 6
+        self.n_slices = 8
         self.threshGrey = 65
         self.crossWidth = 300
         self.cupConfidence = 0.55
