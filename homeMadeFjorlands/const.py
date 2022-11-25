2eae5d37dd5173d3e106e6d5d13b4b4a988b8ab8
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-25 05:02:57 +0100

    pid

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -22,8 +22,8 @@ class Const:
         self.rSpeedPin = 13
         self.rDirPin = 15
 
-        self.kp = 0.05
-        self.kd = 0.013
+        self.kp = 0.04
+        self.kd = 0.1
         self.ki = 0.001
 
         self.n_slices = 3
