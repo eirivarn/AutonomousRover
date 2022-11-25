88e909a47212e42e96b074edb524ba7dca5965ef
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-25 01:15:31 +0100

    ..

--- homeMadeFjorlands/RobotController.py
+++ homeMadeFjorlands/RobotController.py
@@ -24,10 +24,10 @@ class Robot():
         self.camera = CaptureImage(self.const)
         self.motionDetection = MotionDetection(self.const)
         sleep(2)
-        self.task1 = Task1(self.motor, self.lineModule, self.cupModule, self.const)
-        self.task2 = Task2(self.motor, self.lineModule, self.cupModule, self.const)
-        self.task3 = Task3(self.motor, self.lineModule, self.cupModule, self.const)
-        self.task4 = Task4(self.motor, self.lineModule, self.cupModule, self.const)
+        self.task1 = Task1(self.motor, self.lineModule, self.cupModule, self.const, self)
+        self.task2 = Task2(self.motor, self.lineModule, self.cupModule, self.const, self)
+        self.task3 = Task3(self.motor, self.lineModule, self.cupModule, self.const, self)
+        self.task4 = Task4(self.motor, self.lineModule, self.cupModule, self.const, self)
 
         self.n_slices = self.const.n_slices
         self.crossConfidence = np.zeros(self.n_slices)
