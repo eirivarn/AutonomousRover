4396bbcf5b700d50b08995a8e7c9be0d02bb1e4f
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-25 06:37:00 +0100

    ..

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -57,8 +57,8 @@ class Const:
         ##/////////// Servo //////////
         self.open_val = 0.2
         self.close_val = 0.9
-        self.up = 1.
-        self.down = 0.7
+        self.up = 1
+        self.down = 0.6
 
 
         #//////////init Speed/////////
