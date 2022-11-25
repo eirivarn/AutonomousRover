778d0ab9d5e435331e77fcfcc76687304d6d5a8c
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-25 09:26:10 +0100

    speed 8

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -4,7 +4,7 @@ class Const:
     def __init__(self):
         self.isHeadless = False
 
-        self.speed = 12
+        self.speed = 8
         self.turnSpeed = 10
         self.cupDistBuffer = 40
         self.cupPosBuffer = 70
