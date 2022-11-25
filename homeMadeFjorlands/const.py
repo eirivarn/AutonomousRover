d08ac9c1b1eafabe0da0845222ba45594ed10035
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-25 09:03:58 +0100

    ..

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -57,8 +57,8 @@ class Const:
         ##/////////// Servo //////////
         self.open_val = 0.7
         self.close_val = 1
-        self.up = 0.5
-        self.down = 0
+        self.up = 0.9
+        self.down = 0.3
 
 
         #//////////init Speed/////////
