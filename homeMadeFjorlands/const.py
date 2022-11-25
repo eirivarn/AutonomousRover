ed7789b45dbd3ff70c08b3004cf96689c6fdbd9a
larbakke <99260907+larbakke@users.noreply.github.com>
2022-11-25 06:40:01 +0100

    ..

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -4,7 +4,7 @@ class Const:
     def __init__(self):
         self.isHeadless = False
 
-        self.speed = 25
+        self.speed = 35
         self.turnSpeed = 10
         self.cupDistBuffer = 40
         self.cupPosBuffer = 70
