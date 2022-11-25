fc6027c812b2fc5127a91d969bb40350edeed579
eirivarn <97551550+eirivarn@users.noreply.github.com>
2022-11-25 09:06:02 +0100

    updata cam

--- homeMadeFjorlands/const.py
+++ homeMadeFjorlands/const.py
@@ -57,8 +57,8 @@ class Const:
         ##/////////// Servo //////////
         self.open_val = 0.7
         self.close_val = 1
-        self.up = 0.9
-        self.down = 0.4
+        self.up = 1
+        self.down = 0.5
 
 
         #//////////init Speed/////////
